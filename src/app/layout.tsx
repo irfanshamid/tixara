145d14343103e4416f16e7cfaa53368868fdc901
Irfan Hamid <irfansyukri597@gmail.com>
2025-12-09 10:20:49 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,18 @@
 import { Outfit } from 'next/font/google';
 import './globals.css';
-
-import { SidebarProvider } from '@/context/SidebarContext';
-import { ThemeProvider } from '@/context/ThemeContext';
-import { ToastClient } from '@/components/common/ToastClient';
+import { Providers } from './provider';
 
 const outfit = Outfit({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ThemeProvider>
-          <SidebarProvider>{children}</SidebarProvider>
-          <ToastClient />
-        </ThemeProvider>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
---
+++ src/app/provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { SidebarProvider } from "@/context/SidebarContext";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { ToastClient } from "@/components/common/ToastClient";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider>
+      <SidebarProvider>
+        {children}
+        <ToastClient />
+      </SidebarProvider>
+    </ThemeProvider>
+  );
+}
