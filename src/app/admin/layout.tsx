8e534f6865f3d6a2addfd3f5ed3126f78b0fb69c
Irfan Hamid <irfansyukri597@gmail.com>
2025-12-10 20:15:51 +0700

    update

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,5 @@
-import AdminClient from "./layout.client";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   console.log(children);
-  return <AdminClient>hehe</AdminClient>;
+  return children;
 }
